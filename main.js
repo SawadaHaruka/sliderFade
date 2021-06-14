1b9b73ea8c85e3c3202fc5c6a6224209481d7922
SawadaHaruka <65148869+SawadaHaruka@users.noreply.github.com>
2021-06-15 02:41:27 +0900

    rotate dots

--- main.js
+++ main.js
@@ -16,60 +16,29 @@ const init = () => {
     }
 
     //svgエリア
-    const svg = document.getElementById('svg_stage'),
-      NS = svg.getAttribute('xmlns');
-
-    //インジケータ配置
-    for (let j = 0; j < img_num; j++) {
-      let en_width = 0.5;//svg空間の幅100に対しての大きさ
-      let circle = document.createElementNS(NS, 'circle');//円を生成
-      circle.setAttribute('cx', j * 3 + 50 - (en_width * img_index / 2 + en_width / 2 + 2 * img_index / 2)); //真ん中に配置したい
-      circle.setAttribute('cy', 1);
-      circle.setAttribute('r', en_width);
-      svg.appendChild(circle);
-      circle.style.fill = "#bbb";
-      circle.style.cursor = "pointer";
-      circles.push(circle);
-      circle.addEventListener('click', () => {
-        let number = circles.indexOf(circle, 0); //クリックした円を取得
-        state = number;//クリックしたn番目をステートにする
-        changeImg();
-      });
-      circle.addEventListener('mouseover', () => {
-        circle.style.fill = "#c50022";
-      });
-      circle.addEventListener('mouseout', () => {
-        circle.style.fill = "#bbb";
-        if (circle == circles[state]) {
-          circle.style.fill = "#c50022";
-        }
-      });
-
-      let stroke_c = document.createElementNS(NS, 'circle');//円を生成
-      stroke_c.setAttribute('cx', j * 3 + 50 - (en_width * img_index / 2 + en_width / 2 + 2 * img_index / 2)); //真ん中に配置したい
-      stroke_c.setAttribute('cy', 1);
-      stroke_c.setAttribute('r', en_width * 1.8);
-      stroke_c.setAttribute('stroke-width', '0.2');
-      stroke_c.style.fill = 'none';
-      svg.appendChild(stroke_c);
-      strokes.push(stroke_c);
-      // console.log(en_width * 1.8 * 2 * Math.PI);
-    }
+    const svg = document.getElementById('svg_btnL'),
+      NS = svg.getAttribute('xmlns'),
+      svg1 = document.getElementById('svg_btnR'),
+      NS1 = svg1.getAttribute('xmlns'),
+      svg2 = document.getElementById('svg_dots'),
+      NS2 = svg2.getAttribute('xmlns');
 
     //戻る・進むボタン
     {
-      let btnLeft_points = "33.27 1, 35 0, 34.4 1, 35 2 ",
-        btnRight_points = "66.73 1, 65 0,65.6 1, 65 2";
+      let btnLeft_points = "20 50, 100 10, 75 50, 100 90",
+        btnRight_points = "80 50, 0 10, 25 50, 0 90";
       let btnLeft = document.createElementNS(NS, 'polygon');//左：戻るボタン
       btnLeft.setAttribute('points', btnLeft_points);
       svg.appendChild(btnLeft);
-      let btnRight = document.createElementNS(NS, 'polygon');//右：進むボタン
+      let btnRight = document.createElementNS(NS1, 'polygon');//右：進むボタン
       btnRight.setAttribute('points', btnRight_points);
-      svg.appendChild(btnRight);
+      svg1.appendChild(btnRight);
       btns.push(btnLeft, btnRight);
       for (let k = 0; k < 2; k++) {
         btns[k].style.fill = "#c50022";
         btns[k].style.cursor = "pointer";
+        btns[k].style.stroke = "#fff";
+        btns[k].style.strokeWidth = "5px";
         btns[k].addEventListener('click', () => {
           if (btns[k] == btns[0]) {
             if (state == 0) {
@@ -91,9 +60,9 @@ const init = () => {
         target.addEventListener('mouseover', () => {
           let z = () => {
             if (target == btns[0]) {
-              return "32.27 1, 35 0,34.4 1, 35 2"; //左：戻るボタン
+              return "0 50, 100 10, 75 50, 100 90"; //左：戻るボタン
             } else {
-              return "67.73 1, 65 0,65.6 1, 65 2"; //右：進むボタン
+              return "100 50, 0 10, 25 50, 0 90"; //右：進むボタン
             }
           }
           anime({
@@ -122,21 +91,55 @@ const init = () => {
       hover(btns[0]);
       hover(btns[1]);
     }
+    //インジケータ配置
+    for (let j = 0; j < img_num; j++) {
+      let en_width = 0.5;//svg空間の幅100に対しての大きさ
+      let circle = document.createElementNS(NS2, 'circle');//円を生成
+      circle.setAttribute('cx', 1); 
+      circle.setAttribute('cy', j * 3 + 50 - (en_width * img_index / 2 + en_width / 2 + 2 * img_index / 2));
+      circle.setAttribute('r', en_width);
+      svg2.appendChild(circle);
+      circle.style.fill = "#bbb";
+      circle.style.cursor = "pointer";
+      circles.push(circle);
+      circle.addEventListener('click', () => {
+        state = circles.indexOf(circle, 0);//クリックした円をステートにする
+        changeImg();
+      });
+      circle.addEventListener('mouseover', () => {
+        circle.style.fill = "#c50022";
+      });
+      circle.addEventListener('mouseout', () => {
+        circle.style.fill = "#bbb";
+        if (circle == circles[state]) {
+          circle.style.fill = "#c50022";
+        }
+      });
 
+      let stroke_c = document.createElementNS(NS2, 'circle');//線を生成
+      stroke_c.setAttribute('cx', 1); 
+      stroke_c.setAttribute('cy', j * 3 + 50 - (en_width * img_index / 2 + en_width / 2 + 2 * img_index / 2));
+      stroke_c.setAttribute('r', en_width * 1.8);
+      stroke_c.setAttribute('stroke-width', '0.2');
+      stroke_c.style.fill = 'none';
+      svg2.appendChild(stroke_c);
+      strokes.push(stroke_c);
+      // console.log(en_width * 1.8 * 2 * Math.PI);
+    }
   }
 
   //ステートによって画像とインジケータの色が変わる
   const changeImg = () => {
     //全部取得して変更
     for (let l = 0; l < img_num; l++) {
-      circles[l].style.fill = "#bbb";
       images[l].style.opacity = 0;
+      circles[l].style.fill = "#bbb";
       strokes[l].style.stroke = "transparent";
       strokes[l].style.strokeDasharray = "0 5.654";
     }
     //ステートのみ上書き
-    circles[state].style.fill = "#c50022";
     images[state].style.opacity = 1;
+    circles[state].style.fill = "#c50022";
     strokes[state].style.stroke = "#c50022";
     anime({
       targets: [strokes[state]],
